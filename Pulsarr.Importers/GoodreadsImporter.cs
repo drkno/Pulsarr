251df550e741cd2227daa64c78fbea6c810619a7
Jay Harris <jay.harris@outlook.co.nz>
2018-06-27 23:13:48 +1000

    Stubs a Goodreads importer

---
+++ Pulsarr.Importers/GoodreadsImporter.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Threading.Tasks;
+
+namespace Pulsarr.Importers
+{
+    public class GoodreadsImporter : IImporter
+    {
+        public Task Import(string type, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
